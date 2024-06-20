fd837674c3d47be035e863b6b2f7aa102b2237d3
Ren Flores <R.Flores@clearwind.earth>
2024-06-20 19:23:39 +0800

    Clean up files

--- src/settings.py
+++ src/settings.py
@@ -9,8 +9,4 @@ CONFIG_DIR = ROOT_DIR / "src/config"
 PROJ_CRS = "EPSG:4326"
 
 # GCS ID and BQ Dataset IDs
-GCP_PROJ_ID = "geo-retail-data-mart"  # Replace with project ID
-ADM_BQ_ID = GCP_PROJ_ID + ".02_adm_bounds.adm4"
-GRIDS_300m_BQ_ID = (
-    GCP_PROJ_ID + ".01_aoi.grids_wadm_zoom17_300m"
-)  # Replace resolution, if not using bing tiles or h3 remove zoom level from name
+GCP_PROJ_ID = "clearwind"  # Replace with project ID
